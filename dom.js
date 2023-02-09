1213bf4767823fe55430e02cb044b86802ffb63a
vamshikrishna061 <kandagatlavamshikrishna11>
2023-02-09 23:22:12 +0530

    manipulate the DOM

--- dom.js
+++ dom.js
@@ -9,17 +9,17 @@
 //header.style.borderBotton = "solid 3px #000";
 
 
-var items = document.getElementsByClassName('list-group-item');
+//var items = document.getElementsByClassName('list-group-item');
 //console.log(items);
 
 //console.log(items[1]);
-console.log(items[2],);
+//console.log(items[2],);
 //items[1].textContent = 'hello2'
 //for(var i =0;i<items.length;i++){
 
    // items[i].style.fontWeight = 'bold'
 
-items[2].style.backgroundColor = 'green'
+//items[2].style.backgroundColor = 'green'
 
 //QUERy SELECTOR//
 
@@ -29,26 +29,143 @@ items[2].style.backgroundColor = 'green'
 //var input = document.querySelector('input')
 //input.value='hello world';
 
-var item = document.querySelector('.list-group-item')
-item.style.color ='red';
+// var item = document.querySelector('.list-group-item')
+// item.style.color ='red';
 
-var lastItem = document.querySelector
-('.list-group-item:last-child');
-lastItem.style.color = 'blue';
+// var lastItem = document.querySelector
+// ('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
 
-var lastItem = document.querySelector
-('.list-group-item:last-child(2)');
-lastItem.style.color = 'coral';
+// var lastItem = document.querySelector
+// ('.list-group-item:last-child(2)');
+// lastItem.style.color = 'coral';
 
 
-var titles = document.querySelectorAll('.title');
-console.log(titles);
+// var titles = document.querySelectorAll('.title');
+// console.log(titles);
 
-var odd = document.querySelectorAll('li:nth-child(odd');
-var odd = document.querySelectorAll('li:nth-child(odd');
+// var odd = document.querySelectorAll('li:nth-child(odd');
+// var odd = document.querySelectorAll('li:nth-child(odd');
 
-for(var i=0; i<onloadeddata.length;i++){
-    odd[i].style.backgroundColor = '#f4f4f4';
-    Even[i].style.backgroundColor = '#f4f4f4';
+// for(var i=0; i<onloadeddata.length;i++){
+//     odd[i].style.backgroundColor = '#f4f4f4';
+//     Even[i].style.backgroundColor = '#f4f4f4';
+//     var odd = document.querySelectorAll('li:nth-child(odd');
+// var odd = document.querySelectorAll('li:nth-child(odd');
+// var odd = document.querySelectorAll('li:nth-child(odd');
+//  }
 
-}
+
+// TRAVESRING THE DOM//
+
+//var itemList = document.querySelector('#items');
+// parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.background = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode);
+
+//parentElement
+
+// console.log(itemList.parentNode);
+// itemList.parentElement.style.background = '#f4f4f4';
+//  console.log(itemList.parentElement.parentElement);
+
+//childnodes
+
+// console.log(itemList.childNodes);
+
+
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor = 'yellow';
+
+// //LAST first child
+// console.log(item.firstChild);
+// //first Elemt child
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent ='Hello 4';
+
+// next sibilings
+// console.log(itemList.nextSibling);
+
+// //nextEelemt sibiling
+
+// console.log(itemList.nextSibling);
+
+// // pevious sibiling
+// console.log(itemList.previousSibiling);
+// //previous element sibiling
+// console.log(itemList.previousElementsibiling);
+
+// create div
+var newDev = document.createElement('div');
+//add calss
+mewDiv.className ='hello'
+
+//add if
+newDiv.id = 'hello1';
+
+//add attr
+newDiv.setAttribute('title','hello world')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
